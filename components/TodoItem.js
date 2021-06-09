1f5a5a8b2311a27e0d7ca711b79bb3db92002019
Ahmad Alfan <aalf4nz@gmail.com>
2021-06-09 09:56:22 +0700

    Add x icon to the card

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TodoItem({ item, pressHandler }) {
-    return (
-        <TouchableOpacity
-            onPress={() => pressHandler(item.key)}
-            style={styles.item}
-        >
-            <Text style={styles.text}>{item.text}</Text>
-        </TouchableOpacity>
-    );
+	return (
+		<TouchableOpacity
+			onPress={() => pressHandler(item.key)}
+			style={styles.item}
+		>
+			<Text style={styles.text}>{item.text}</Text>
+			<Ionicons
+				name='close'
+				size={32}
+				color='rgba(255, 255, 255, 0.24)'
+			/>
+		</TouchableOpacity>
+	);
 }
 
 const styles = StyleSheet.create({
-    item: {
-        padding: 32,
-        marginBottom: 16,
-        backgroundColor: 'rgba(255, 255, 255, 0.11)',
-        borderRadius: 8,
-    },
-    text: {
-        color: '#fff',
-        fontSize: 16,
-    },
+	item: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 32,
+		marginBottom: 16,
+		backgroundColor: 'rgba(255, 255, 255, 0.11)',
+		borderRadius: 8,
+	},
+	text: {
+		color: '#fff',
+		fontSize: 16,
+	},
 });
